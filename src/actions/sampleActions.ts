29500712082a57451da0076cf0ec6777318a2d5c
s11142 <shotanagaizumi@gmail.com>
2025-02-23 21:31:16 +0900

    useActionStateを使って、フォームアクションの結果に応じてstateを更新する

--- src/actions/sampleActions.ts
+++ src/actions/sampleActions.ts
@@ -1,6 +1,14 @@
 "use server"; // このファイルに定義された非同期関数がServer Actionsとして認識されるようになる
 
-export const createTask = async (taskId: number, formData: FormData) => {
+export interface FormState {
+  error: string;
+}
+
+export const createTask = async (
+  taskId: number,
+  prevState: FormState,
+  formData: FormData
+) => {
   // DBにタスクを作成
   console.log("タスクを作成しました");
 
@@ -8,4 +16,8 @@ export const createTask = async (taskId: number, formData: FormData) => {
   console.log(formData.get("name"));
 
   console.log(`taskId: ${taskId}`);
+  // Server Actionsでエラーが発生したと仮定.
+  // useActionStateはcreateTaskの戻り値を自動的にstateに設定するため、stateを直接更新する必要はない.
+  // prevState.error = "エラーが発生しました!"みたいなことはする必要がないということ↑
+  return { error: "エラーが発生しました!" };
 };
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,16 +1,26 @@
-import { createTask } from "@/actions/sampleActions";
+"use client";
+
+import { createTask, FormState } from "@/actions/sampleActions";
+import { useActionState } from "react";
 
 const ServerActionsPage = () => {
   const taskId = 1;
   //taskIdを引数に含む新しい関数を作成. Server Actions側で定義した第一引数と紐付けている
   const createTaskWithTaskId = createTask.bind(null, taskId);
+  const initialState: FormState = { error: "" };
+  // state: Server Actionsの戻り値, formAction: Server Actionsと同じ動きをする関数
+  const [state, formAction] = useActionState(
+    createTaskWithTaskId,
+    initialState
+  );
   return (
     <div>
-      <form action={createTaskWithTaskId}>
+      <form action={formAction}>
         <input type="text" id="name" name="name" className="bg-gray-200" />
         <button type="submit" className="bg-gray-400 ml-2">
           送信
         </button>
+        <div>{state.error}</div>
       </form>
     </div>
   );
